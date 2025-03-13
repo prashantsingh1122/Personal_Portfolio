1859c55bf4f66f774757e4e7f87a5c748d421304
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-03-14 02:36:31 +0530

    done with css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import { LettersPullUp } from "./LettersPullup";
 import { ForHeading } from "./ForHeading";
 import CardSwitcher from "./CardSwitcher";
 import TiltedCard from "./TiltedCard";
+import GridMotion from "./GridMotion";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faGithub,
@@ -48,6 +49,34 @@ const Home = () => {
       description: "Badminton is another sport I love. It helps me stay fit and improves my reflexes."
     },
   ];
+  //  FOR SKILLS PART    -----------note: you'll need to make sure the parent container of this component is sized properly  
+const items = [
+  'Item 1',
+  <div key='jsx-item-1'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 2',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 4',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 5',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 7',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 8',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 10',
+  <div key='jsx-item-3'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 11',
+  <div key='jsx-item-2'>Custom JSX Content</div>,
+  'Item 13',
+  <div key='jsx-item-4'>Custom JSX Content</div>,
+  'https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'Item 14',
+  // Add more items as needed
+];
 
   return (
     <motion.div
@@ -252,6 +281,9 @@ const Home = () => {
               </div>
             ))}
           </div>
+        </div>
+        <div>
+        <GridMotion items={items} />
         </div>
 
     </motion.div>
